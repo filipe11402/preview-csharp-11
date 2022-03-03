a960db36b71002205273cf118f90ac0c66b01441
filipe11402 <filipe11402@outlook.pt>
2022-03-03 23:06:53 +0000

    added file scoped namespace declaration

---
+++ src/CSharpTen/ScopedNamespace/FileNamespace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTen.ScopedNamespace;
+
+class MySampleClass 
+{
+    private string Name { get => "I Belong to Sample Class"; }
+
+    public MySampleClass()
+    {
+    }
+}
+
+enum SampleEnum 
+{
+    Start,
+    End
+}
